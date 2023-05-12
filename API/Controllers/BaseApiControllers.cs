36a7c10ab82d17f26787b674bd95925eb1c3ea04
BABEL-SI\margarita.martinez <margarita.martinez@babelgroup.com>
2023-05-12 14:33:58 +0200

    End of section 13

--- API/Controllers/BaseApiControllers.cs
+++ API/Controllers/BaseApiControllers.cs
@@ -1,8 +1,10 @@
 
 using Microsoft.AspNetCore.Mvc;
+using API.Helpers;
 
 namespace API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")] 
     public class BaseApiControllers : ControllerBase
