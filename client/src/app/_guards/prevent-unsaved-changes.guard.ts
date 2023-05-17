7014e66d9c2e06563445477a74d206e1e94b718f
BABEL-SI\margarita.martinez <margarita.martinez@babelgroup.com>
2023-05-17 15:08:27 +0200

    End of section 18

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import {  CanDeactivate } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
-//import { ConfirmService } from '../_services/confirm.service';
+import { ConfirmService } from '../_services/confirm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<MemberEditComponent> {
- // constructor(private confirmService: ConfirmService) {}
+  constructor(private confirmService: ConfirmService) {}
 
-  canDeactivate(component: MemberEditComponent): boolean {
+  canDeactivate(component: MemberEditComponent): Observable<boolean> {
     if (component.editForm?.dirty) {
-      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
-      //return this.confirmService.confirm()
+      return this.confirmService.confirm()
     }
-    return true;
+    return of(true);
   }
   
 }
